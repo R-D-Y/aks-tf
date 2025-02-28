2d4ef0453744cd96e8d0d00d512b1f9a64845576
Rémi... et pas Rémy <102509252+R-D-Y@users.noreply.github.com>
2025-02-28 14:45:31 -0500

    Update t2.py

--- t2.py
+++ t2.py
@@ -1,17 +1,8 @@
-clean_text() {
-    echo "$1" | sed 's/[^a-zA-Z0-9_-]//g'
-}
-
-
-# Nettoyage des noms pour éviter les erreurs de caractères spéciaux
-service_instance_name=$(clean_text "$service_instance_name")
-space_name=$(clean_text "$space_name")
-
-
-
-
-
-
-
-
-
+# Déterminer si la BD a un problème d'allocation
+if [[ "$allocatedPercentage" -gt 100 ]]; then
+    probleme="Oui - Allocation dépasse 100%"
+elif [[ "$allocatedPercentage" -gt 90 ]]; then
+    probleme="Oui - Allocation dépasse 90%"
+else
+    probleme="BD saine"
+fi
